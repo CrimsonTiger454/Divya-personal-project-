1a93037332b1380de1b506265072f62b060a19a6
robyn <robyn@s56.net>
2018-06-11 12:12:17 -0600

    consolelogs everyeh

--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -38,7 +38,7 @@ export class Home extends Component {
         this.props.getUser();
         let goalInfo = this.props.getGoalInfo();
         goalInfo.then( (res) => {
-        
+            console.log(res);
             this.setState({staticCalories: res.value.caloriegoal})
             this.getLoggedFood();
         })
@@ -47,7 +47,7 @@ export class Home extends Component {
 
     getLoggedFood() {
         axios.get('/getLoggedFood?foodtime=today').then( res => {
-        
+            console.log(res);
             this.setState({foodLog: res.data});
             this.calculateTotals();
         })
@@ -73,7 +73,7 @@ export class Home extends Component {
 
     removeFoodItem (id) {
         axios.delete(`/removeFood/${id}`).then((res) => {
-        
+            console.log(res);
             this.getLoggedFood();
         })
     }
@@ -105,9 +105,9 @@ export class Home extends Component {
             this.setState({label: '', fats: '', carbs: '', protein: '', calories: ''})
         } else {
             axios.put('/addNewFood', {label, fats, carbs, protein, calories}).then(res => {
-            
+                console.log(res);
                 axios.get('/getLoggedFood?foodtime=today').then( res => {
-                
+                    console.log(res);
                     this.setState({foodLog: res.data});
                     this.calculateTotals();
                 })
